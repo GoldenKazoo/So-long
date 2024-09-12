23014377ffbc6869856c39ef3b75d430895f8a15
Zakarya Chagar <zchagar@paul-f4Ar1s4.clusters.42paris.fr>
2024-09-12 20:00:54 +0200

    Leaks fixed, starting pathfinding

---
+++ pathfinding/aStar_utils.c
@@ -0,0 +1,19 @@
+#include "parsing.h"
+
+t_node	**ft_to_nodetab(t_map *map)
+{
+	int		i;
+	int		j;
+	t_node	**nodetab;
+
+	i = 0;
+	j = 0;
+
+	while (map -> height)
+	{
+		while (map -> width)
+		{
+
+		}
+	}
+}
