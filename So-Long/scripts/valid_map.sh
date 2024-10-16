6e87caf654a1fb89cdfa061f86831c514648da05
Zakarya Chagar <zchagar@made-f0Ar3s1.clusters.42paris.fr>
2024-10-16 05:25:08 +0200

    So_long 100%?

---
+++ So-Long/scripts/valid_map.sh
@@ -0,0 +1,45 @@
+./so_long map/maps_valid/ok.ber
+valgrind --leak-check=full --show-leak-kinds=all ./so_long map/maps_valid/ok.ber
+echo "==================================================================="
+./so_long map/maps_valid/ok1.ber
+valgrind --leak-check=full --show-leak-kinds=all ./so_long map/maps_valid/ok1.ber
+echo "==================================================================="
+./so_long map/maps_valid/ok2.ber
+valgrind --leak-check=full --show-leak-kinds=all ./so_long map/maps_valid/ok2.ber
+echo "==================================================================="
+./so_long map/maps_valid/ok3.ber
+valgrind --leak-check=full --show-leak-kinds=all ./so_long map/maps_valid/ok3.ber
+echo "==================================================================="
+./so_long map/maps_valid/ok4.ber
+valgrind --leak-check=full --show-leak-kinds=all ./so_long map/maps_valid/ok4.ber
+echo "==================================================================="
+./so_long map/maps_valid/ok5.ber
+valgrind --leak-check=full --show-leak-kinds=all ./so_long map/maps_valid/ok5.ber
+echo "==================================================================="
+./so_long map/maps_valid/ok6.ber
+valgrind --leak-check=full --show-leak-kinds=all ./so_long map/maps_valid/ok6.ber
+echo "==================================================================="
+./so_long map/maps_valid/ok7.ber
+valgrind --leak-check=full --show-leak-kinds=all ./so_long map/maps_valid/ok7.ber
+echo "==================================================================="
+./so_long map/maps_valid/ok8.ber
+valgrind --leak-check=full --show-leak-kinds=all ./so_long map/maps_valid/ok8.ber
+echo "==================================================================="
+./so_long map/maps_valid/ok9.ber
+valgrind --leak-check=full --show-leak-kinds=all ./so_long map/maps_valid/ok9.ber
+echo "==================================================================="
+./so_long map/maps_valid/ok10.ber
+valgrind --leak-check=full --show-leak-kinds=all ./so_long map/maps_valid/ok10.ber
+echo "==================================================================="
+./so_long map/maps_valid/ok12.ber
+valgrind --leak-check=full --show-leak-kinds=all ./so_long map/maps_valid/ok12.ber
+echo "==================================================================="
+./so_long map/maps_valid/ok13.ber
+valgrind --leak-check=full --show-leak-kinds=all ./so_long map/maps_valid/ok13.ber
+echo "==================================================================="
+./so_long map/maps_valid/ok14.ber
+valgrind --leak-check=full --show-leak-kinds=all ./so_long map/maps_valid/ok14.ber
+echo "==================================================================="
+./so_long map/maps_valid/ok15.ber
+valgrind --leak-check=full --show-leak-kinds=all ./so_long map/maps_valid/ok15.ber
+echo "==================================================================="
